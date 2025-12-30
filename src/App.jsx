afb76492d890a7d871a35a4be3be97efd1fc2526
sachin-Rahangdale <rahangdalesachin02@gmail.com>
2025-12-30 13:08:59 +0530

    Added functionality to change colors

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,52 @@ import React, { useState } from 'react'
 import './App.css'
 const App = () =>{
 
+  const [show, setShow] = useState(true);
   let [counter, setCounter]= useState(0);
-  function increaseCounter(){
-    setCounter(counter = counter + 1)
+  const [color,setColor]=useState('white');
+  function increaseCounter(num){
+    setCounter(counter = counter + num)
+    console.log(counter);
+    setColor('blue');
+  }
+  function decreaseCounter(num){
+    setCounter(counter = counter -num);
+    console.log(counter);
+    setColor('red')
+  }
+  const reset=()=>{
+    setCounter(counter = 0);
     console.log(counter);
   }
-  function decreaseCounter(){
-    setCounter(counter = counter -1)
-    console.log(counter);
-  
+  const onInputChange=(ele)=>{
+    console.log(ele);
+    setColor(ele.target.value);
   }
+
 
   return (
     <>
-    <button onClick={increaseCounter}> click me ++ </button><p></p>
-    {counter}
-    <button onClick={decreaseCounter}> click me -- </button>
+    {show &&
+    <div style={{backgroundColor:'green', margin:'10px',marginRight:'70%', padding :'10px'}}>
+      <button onClick={()=>increaseCounter(5)} > click me ++ </button><p></p>
+    {/* when we dont have parameter we can only use name of function without () */}
+    <h1 style={{color : color}}>{counter}</h1>
     
-     
+    <button disabled={counter ==0 || counter - 10 <= 0 } onClick={()=>decreaseCounter(10)}> click me -- </button>
+    {/*this is how we pass parameter in function we use ()=> */}
+
+    <button onClick={reset}> Reset to Zero </button>
+    </div>
+    
+}
+
+    <button onClick={()=> setShow(!show)} 
+    type='button'
+     >{show ? 'Hide': 'Show'}</button>
+     <input type='text' onChange={onInputChange}/>
+  
     </>
+
   )
 }
 
